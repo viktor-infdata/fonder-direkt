2e1a9caf9ae63607b892d8962324e855ed9c5165
Viktor Karlsson <vikar@osl.infront.as>
2018-11-13 15:27:51 +0100

    Switch to scss and purgecss

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
-import './all.sass'
+import './all.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
