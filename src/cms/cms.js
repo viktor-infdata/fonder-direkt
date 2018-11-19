ca3a716142c80d70db25d2aba3e122f355e038a4
Viktor Karlsson <vikar@osl.infront.as>
2018-11-19 14:07:35 +0100

    refactor YoutTube wdiget cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,55 +6,54 @@ import VideoPostPreview from './preview-templates/VideoPostPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('video', VideoPostPreview)
 CMS.registerEditorComponent({
-    // Internal id of the component
-    id: "youtube",
-    // Visible label
-    label: "Youtube",
-    // Fields the user need to fill out when adding an instance of the component
-    fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
-    // Pattern to identify a block as being an instance of this component
-    pattern: /^youtube (\S+)$/,
-    // Function to extract data elements from the regexp match
-    fromBlock: function(match) {
-      return {
-        id: match[1]
-      };
-    },
-    // Function to create a text block from an instance of this component
-    toBlock: function(obj) {
-      return '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item mb-3" src="https://www.youtube.com/embed/'+ obj.id + '?rel=0" allowfullscreen></iframe></div>';
-    },
-    // Preview output for this component. Can either be a string or a React component
-    // (component gives better render performance)
-    toPreview: function(obj) {
-      return (
-        '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
-      );
-    }
-  });
+  id: "youtube",
+  label: "Youtube",
+  fields: [{
+    name: 'id',
+    label: 'YouTube Video ID',
+    widget: 'string'
+  }],
+  pattern: /^<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item mb-3" src="https:\/\/www.youtube.com\/embed\/(.*)?rel=0" allowfullscreen><\/iframe><\/div>/,
+  fromBlock: function(match) {
+    return {
+      id: match[1],
+    };
+  },
+  toBlock: function(obj) {
+    return (
+      '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item mb-3" src="https://www.youtube.com/embed/'+ obj.id + '?rel=0" allowfullscreen></iframe></div>'
+    );
+  },
+  toPreview: function(obj) {
+    return (
+      '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item mb-3" src="https://www.youtube.com/embed/'+ obj.id + '?rel=0" allowfullscreen></iframe></div>'
+    );
+  },
+});
   CMS.registerEditorComponent({
     id: "caption",
     label: "Caption",
     fields: [{
-      name: 'element',
+      name: 'caption',
       label: 'Image Caption',
       widget: 'string'
     }],
     pattern: /^<span class="image-caption">(.*)<\/span>/,
     fromBlock: function(match) {
       return {
-        element: match[1],
+        caption: match[1],
       };
     },
     toBlock: function(obj) {
       return (
-        '<span class="image-caption">' + obj.element + '</span>'
+        '<span class="image-caption">' + obj.caption + '</span>'
       );
     },
     toPreview: function(obj) {
       return (
-        '<span class="image-caption">' + obj.element + '</span>'
+        '<span class="image-caption">' + obj.caption + '</span>'
       );
     },
   });
