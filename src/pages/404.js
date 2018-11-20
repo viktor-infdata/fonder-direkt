2e0e119ccadd4a87b22d5b0ca856052761ae4e65
Viktor Karlsson <viktor.karlsson@inquiry.se>
2018-11-20 14:36:49 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section class="hero is-light is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title mb-5">
+            Oops verkar som att du har hittat en sida som inte finns&hellip;
+          </h1>
+          <h2 class="subtitle">
+            Men inga problem, gå gärna till <Link to="/">startsidan</Link> och hitta dina nyheter :-)
+          </h2>
+        </div>
+      </div>
+    </section>
     </div>
   </Layout>
 )
