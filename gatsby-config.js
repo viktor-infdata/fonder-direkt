fecc6cb91e50fea9509a2096f2a7a8215ff23781
Viktor Karlsson <vikar@osl.infront.as>
2018-11-21 16:48:33 +0100

    fallback relative images

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images-v2',
+            resolve: 'gatsby-remark-relative-images',
             options: {
               name: 'uploads',
             },
