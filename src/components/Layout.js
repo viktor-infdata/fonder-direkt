13bb57eb0a31eeb8e1a2f378d29d625a01a92c74
Viktor Karlsson <vikar@osl.infront.as>
2018-11-21 17:06:59 +0100

    Relative paths updated

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,13 +13,13 @@ const TemplateWrapper = ({ children }) => (
       <html lang="se" />
       <meta name="description" content="Fonder Direkt är nordens ledande webbplats om fonder som produceras av Nyhetsbyrån Direkts fondredaktion i syfte att bredda kunskapen om fonder och öka genomlysningen av denna marknad." />
       <meta name="copyright" content="Fonder Direkt är en del av Aktiebolaget Nyhetsbyrån Direkt" />
-      <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"  />
-      <link rel="mask-icon" href="safari-pinned-tab.svg" color="#037184" />
-      <link rel="shortcut icon" href="favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"  />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#037184" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="msapplication-TileColor" content="#00aba9" />
-      <meta name="msapplication-config" content="browserconfig.xml" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
     </Helmet>
     <Navbar />
     <div>{children}</div>
