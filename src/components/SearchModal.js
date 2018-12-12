739653700acf7465369e4a2750e5283a950c1656
Viktor Karlsson <viktor.karlsson@inquiry.se>
2018-12-12 16:00:36 +0100

    Search Design

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -51,7 +51,7 @@ class SearchModal extends Component {
             <span className="tip">[ESC]</span>
           </a>
           <div className="section">
-            <div className="container pt-5">
+            <div className="container">
               <div className="columns is-centered">
                 <div className="column is-8">
                   <SearchInit />
