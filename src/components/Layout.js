3e0b216ea71a8cfe15cfc6627515c79fab84c827
Viktor Karlsson <vikar@osl.infront.as>
2018-12-19 16:43:53 +0100

    Move ScrollUp and Consenst to footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,8 +4,6 @@ import Helmet from 'react-helmet'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import './all.scss'
-import CookieConsent, { Cookies } from "react-cookie-consent";
-import ScrollUpButton from "react-scroll-up-button";
 
 const TemplateWrapper = ({ children }) => (
   <React.Fragment>
@@ -41,25 +39,6 @@ const TemplateWrapper = ({ children }) => (
       </div>
       <Footer />
     </div>
-    <CookieConsent
-      contentClasses="cookieConsentContainer"
-      buttonClasses="button is-small is-black"
-      disableStyles={true}
-      buttonText="Acceptera"
-    >
-      <p className="is-size-7 pr-4">
-        På denna webbplats använder vi cookies för att förbättra funktionaliteten för dig som användare.{" "}
-      </p>
-    </CookieConsent>
-    <ScrollUpButton
-      ContainerClassName="scroll-to-top"
-      TransitionClassName="scroll-to-top-transition">
-        <svg xmlns="http://www.w3.org/2000/svg">
-          <title>Scroll to top</title>
-          <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/>
-          <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
-    </ScrollUpButton>
   </React.Fragment>
 )
 
