075998c0b6df3e98544cf5500c8975e80781a3d4
Viktor Karlsson <viktor.karlsson@inquiry.se>
2019-01-02 15:28:10 +0100

    Search test

--- gatsby-config.js
+++ gatsby-config.js
@@ -178,14 +178,13 @@ module.exports = {
       resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
       options: {
         // Fields to index
-        fields: [`title`, `tags`, 'rawMarkdownBody'],
+        fields: [`title`, `tags`],
         // How to resolve each field`s value for a supported node type
         resolvers: {
           // For any node of type MarkdownRemark, list how to resolve the fields` values
           MarkdownRemark: {
             title: node => node.frontmatter.title,
             tags: node => node.frontmatter.tags,
-            rawMarkdownBody: node => node.rawMarkdownBody,
             slug: node => node.fields.slug,
           },
         },
