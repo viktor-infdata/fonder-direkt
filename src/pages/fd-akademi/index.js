104116af919abc14b3cf7c0005f5f05e0f8652c0
Viktor Karlsson <vikar@osl.infront.as>
2019-10-04 15:47:28 +0200

    Dep bump and Akademi Update

--- src/pages/fd-akademi/index.js
+++ src/pages/fd-akademi/index.js
@@ -66,103 +66,118 @@ class IndexPage extends React.Component {
 
     return (
       <Layout>
-        <section className="hero is-light">
-          <Helmet title="FD Akademi | Fonder Direkt">
-            <meta
-              name="description"
-              content="Senaste utbildningsmaterialet från Fonder Direkt, en plattform där du kan hitta information, läsa nyheter och ta del av kommunikation om fonder."
-            />
-            <meta property="og:title" content="FD Akademi | Fonder Direkt" />
-            <meta
-              property="og:url"
-              content="https://fonderdirekt.se/fd-akademi/"
-            />
-            <meta
-              property="og:description"
-              content="Senaste utbildningsmaterialet från Fonder Direkt, en plattform där du kan hitta information, läsa nyheter och ta del av kommunikation om fonder."
-            />
-          </Helmet>
-          <div className="hero-body">
-            <div className="container">
-              <div className="columns is-multiline">
-                {chunk(posts.slice(0, this.state.educationPostsToShow), 9).map(
-                  (chunk, i) => (
-                    <React.Fragment key={`chunk-${i}`}>
-                      {chunk.map(node => (
-                        <div
-                          className="column is-one-third-desktop is-half-tablet"
-                          key={node.id}
-                        >
-                          <div className="card has-equal-height">
-                            <div className="card-image">
-                              {node.frontmatter.videoId != null ? (
-                                <div className="embed-responsive embed-responsive-16by9">
-                                  <iframe
-                                    title={node.frontmatter.videoId}
-                                    className="embed-responsive-item"
-                                    src={
-                                      'https://www.youtube.com/embed/' +
-                                      node.frontmatter.videoId +
-                                      '?rel=0'
+        <React.Fragment>
+          <section className="hero is-dark is-bold">
+            <div className="hero-body">
+              <div className="container">
+                <h1 class="title">
+                  FD AKADEMI
+                </h1>
+                <article class="message is-fd-akademi">
+                  <div class="message-body">
+                    FD Akademi erbjuder utbildning och förståelse av finansiella termer, ämnen och teman relaterade till fonder samt andra finansiella investeringsprodukter. FD Akademi vill bryta barriärerna när det gäller finans, och ge alla som lära sig mer den möjligheten via vårt innehåll. Det har aldrig varit viktigare att kunna ta kontroll över sina ekonomiska beslut, och FD Akademi vill tillsammans med Fonder Direkt tillhandahålla den information du behöver för att kunna göra detta.
+                  </div>
+                </article>
+              </div>
+            </div>
+          </section>
+          <section className="hero is-light">
+            <Helmet title="FD Akademi | Fonder Direkt">
+              <meta
+                name="description"
+                content="Senaste utbildningsmaterialet från Fonder Direkt, en plattform där du kan hitta information, läsa nyheter och ta del av kommunikation om fonder."
+              />
+              <meta property="og:title" content="FD Akademi | Fonder Direkt" />
+              <meta
+                property="og:url"
+                content="https://fonderdirekt.se/fd-akademi/"
+              />
+              <meta
+                property="og:description"
+                content="Senaste utbildningsmaterialet från Fonder Direkt, en plattform där du kan hitta information, läsa nyheter och ta del av kommunikation om fonder."
+              />
+            </Helmet>
+            <div className="hero-body">
+              <div className="container">
+                <div className="columns is-multiline">
+                  {chunk(posts.slice(0, this.state.educationPostsToShow), 9).map(
+                    (chunk, i) => (
+                      <React.Fragment key={`chunk-${i}`}>
+                        {chunk.map(node => (
+                          <div
+                            className="column is-one-quarter-desktop is-half-tablet"
+                            key={node.id}
+                          >
+                            <div className="card has-equal-height">
+                              <div className="card-image">
+                                {node.frontmatter.videoId != null ? (
+                                  <div className="embed-responsive embed-responsive-16by9">
+                                    <iframe
+                                      title={node.frontmatter.videoId}
+                                      className="embed-responsive-item"
+                                      src={
+                                        'https://www.youtube.com/embed/' +
+                                        node.frontmatter.videoId +
+                                        '?rel=0'
+                                      }
+                                      allowFullScreen
+                                    />
+                                  </div>
+                                ) : (
+                                  <Img
+                                    fluid={
+                                      node.frontmatter.image.childImageSharp.fluid
                                     }
-                                    allowFullScreen
+                                    alt={node.frontmatter.title}
                                   />
-                                </div>
-                              ) : (
-                                <Img
-                                  fluid={
-                                    node.frontmatter.image.childImageSharp.fluid
-                                  }
-                                  alt={node.frontmatter.title}
-                                />
-                              )}
-                            </div>
-                            <div className="card-content">
-                              <div className="content">
-                                <h5 className="is-size-7 mb-0">
-                                  <small className="has-text-grey">
-                                    <time dateTime={node.frontmatter.date}>
-                                      {node.frontmatter.date}
-                                    </time>
-                                  </small>
-                                  {node.frontmatter.english === true && (
-                                    <span className="has-text-grey">
-                                      {' '}
-                                      &bull;{' '}
-                                      <small className="has-text-grey">
-                                        <svg
-                                          role="img"
-                                          viewBox="0 0 24 24"
-                                          className="is-language"
-                                          xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                          <title>Language</title>
-                                          <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm6.93 6h-2.95c-.32-1.25-.78-2.45-1.38-3.56 1.84.63 3.37 1.91 4.33 3.56zM12 4.04c.83 1.2 1.48 2.53 1.91 3.96h-3.82c.43-1.43 1.08-2.76 1.91-3.96zM4.26 14C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2 0 .68.06 1.34.14 2H4.26zm.82 2h2.95c.32 1.25.78 2.45 1.38 3.56-1.84-.63-3.37-1.9-4.33-3.56zm2.95-8H5.08c.96-1.66 2.49-2.93 4.33-3.56C8.81 5.55 8.35 6.75 8.03 8zM12 19.96c-.83-1.2-1.48-2.53-1.91-3.96h3.82c-.43 1.43-1.08 2.76-1.91 3.96zM14.34 14H9.66c-.09-.66-.16-1.32-.16-2 0-.68.07-1.35.16-2h4.68c.09.65.16 1.32.16 2 0 .68-.07 1.34-.16 2zm.25 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95c-.96 1.65-2.49 2.93-4.33 3.56zM16.36 14c.08-.66.14-1.32.14-2 0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2h-3.38z" />
-                                        </svg>
-                                        ENGLISH
-                                      </small>
-                                    </span>
-                                  )}
-                                </h5>
-                                <h1 className="is-size-5 mt-1 mb-2">
-                                  <Link to={node.fields.slug}>
-                                    {node.frontmatter.title}
-                                  </Link>
-                                </h1>
-                                <p>{node.excerpt}</p>
+                                )}
                               </div>
+                              <div className="card-content">
+                                <div className="content">
+                                  <h5 className="is-size-7 mb-0">
+                                    <small className="has-text-grey">
+                                      <time dateTime={node.frontmatter.date}>
+                                        {node.frontmatter.date}
+                                      </time>
+                                    </small>
+                                    {node.frontmatter.english === true && (
+                                      <span className="has-text-grey">
+                                        {' '}
+                                        &bull;{' '}
+                                        <small className="has-text-grey">
+                                          <svg
+                                            role="img"
+                                            viewBox="0 0 24 24"
+                                            className="is-language"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                          >
+                                            <title>Language</title>
+                                            <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm6.93 6h-2.95c-.32-1.25-.78-2.45-1.38-3.56 1.84.63 3.37 1.91 4.33 3.56zM12 4.04c.83 1.2 1.48 2.53 1.91 3.96h-3.82c.43-1.43 1.08-2.76 1.91-3.96zM4.26 14C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2 0 .68.06 1.34.14 2H4.26zm.82 2h2.95c.32 1.25.78 2.45 1.38 3.56-1.84-.63-3.37-1.9-4.33-3.56zm2.95-8H5.08c.96-1.66 2.49-2.93 4.33-3.56C8.81 5.55 8.35 6.75 8.03 8zM12 19.96c-.83-1.2-1.48-2.53-1.91-3.96h3.82c-.43 1.43-1.08 2.76-1.91 3.96zM14.34 14H9.66c-.09-.66-.16-1.32-.16-2 0-.68.07-1.35.16-2h4.68c.09.65.16 1.32.16 2 0 .68-.07 1.34-.16 2zm.25 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95c-.96 1.65-2.49 2.93-4.33 3.56zM16.36 14c.08-.66.14-1.32.14-2 0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2h-3.38z" />
+                                          </svg>
+                                          ENGLISH
+                                        </small>
+                                      </span>
+                                    )}
+                                  </h5>
+                                  <h1 className="is-size-5 mt-1 mb-2">
+                                    <Link to={node.fields.slug}>
+                                      {node.frontmatter.title}
+                                    </Link>
+                                  </h1>
+                                  <p>{node.excerpt}</p>
+                                </div>
+                              </div>
+                              <footer className="card-footer">
+                                <Link
+                                  className="card-footer-item button is-dark is-fullwidth is-feature-button"
+                                  to={node.fields.slug}
+                                >
+                                  Gå till FD Akademi
+                                </Link>
+                              </footer>
                             </div>
-                            <footer className="card-footer">
-                              <Link
-                                className="card-footer-item button is-dark is-fullwidth is-feature-button"
-                                to={node.fields.slug}
-                              >
-                                Gå till FD Akademi
-                              </Link>
-                            </footer>
                           </div>
-                        </div>
-                      ))}
+                        ))}
                     </React.Fragment>
                   )
                 )}
@@ -190,6 +205,7 @@ class IndexPage extends React.Component {
             </div>
           </div>
         </section>
+        </React.Fragment>
       </Layout>
     )
   }
